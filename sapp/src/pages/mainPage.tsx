e21bccccea7c0f4e87415f2918303cf09ac8b622
KJ Lee <kunnyjlee@gmail.com>
2024-12-26 21:56:01 -0800

    fixed the popup for editing shifts

--- sapp/src/pages/mainPage.tsx
+++ sapp/src/pages/mainPage.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import "./mainStyle.css";
-import { ScheduleItem } from "../types";
+import { editShift, ScheduleItem } from "../types";
 import Popup from "../components/popup";
 
 const MainPage: React.FC = () => {
     const [selectedDay, setSelectedDay] = useState<string | null>(null);
     const[popupVisible, setPopupVisible] = useState(false);
-    // const [editShift, setEditShift] = useState<{ day: string; index: number; data: ScheduleItem } | null>(null);
+    const [selectedShift, setSelectedShift] = useState<editShift | null>(null);
+
+
     const [shifts, setShifts] = useState<{[key: string]: ScheduleItem[] }>({
         Monday: [],
         Tuesday: [],
@@ -17,9 +19,9 @@ const MainPage: React.FC = () => {
         Sunday: [],
     });
 
-
     let handleDayClick = (day: string) => {
         setSelectedDay(day);
+        setSelectedShift(null);
         setPopupVisible(true);
     }
     const addShift = (day: string, shift: ScheduleItem) => {
@@ -37,11 +39,24 @@ const MainPage: React.FC = () => {
         }));
     }
 
-    const handleEditClick = (day: string, index: number) => (event: React.MouseEvent<HTMLLIElement>) => {
+    const handleShiftClick = (day: string, index: number) => (event: React.MouseEvent<HTMLLIElement>) => {
         event.stopPropagation(); // Prevent parent click handler
-        const shift = shifts[day][index];
-        // setEditShift({day, index, data: shift});
+        const selectedShiftData = shifts[day][index];
+        setSelectedShift({
+            ...selectedShiftData,
+            day,
+            index,
+        })
         setPopupVisible(true);
+    }
+    const handleUpdateShift = (updateItem: editShift) =>  {
+        const updatedShifts = {...shifts};
+        if(selectedShift){
+            updatedShifts[selectedShift.day][selectedShift.index] = updateItem;
+        }
+        setShifts(updatedShifts); // Update state with new/edited shift
+        setPopupVisible(false); // Close the popup
+        setSelectedShift(null); // Reset selection
     };
     const cslog = () => {
         console.log(shifts);
@@ -58,7 +73,7 @@ const MainPage: React.FC = () => {
                             <h3>{day}</h3>
                             <ul>
                                 {shifts[day].map((shift, index) => (
-                                <li key={index} className = 'shift' onClick={handleEditClick(day, index)}>
+                                <li key={index} className = 'shift' onClick={handleShiftClick(day, index)}>
                                     <button 
                                     className = 'del-btn' 
                                     onClick={(e) => {
@@ -81,6 +96,8 @@ const MainPage: React.FC = () => {
             setPopupVisible={setPopupVisible}
             selectedDay={selectedDay}
             addShift={addShift}
+            selectedShift={selectedShift}
+            handleUpdateShift={handleUpdateShift}
           />
         )}
         <button onClick={cslog}>button</button>
